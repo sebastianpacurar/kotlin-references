62c79899cc6208ce73dd921e1401d10c0e8bb2e4
Sebastian Pacurar <pacurar.sebastian90@gmail.com>
2024-05-12 23:03:44 +0300

    switch dependency management to version catalog

--- basics/build.gradle.kts
+++ basics/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
+    alias(libs.plugins.serialization) apply false
 }
 
 group = "kotlin.references"
@@ -20,21 +20,21 @@ kotlin {
 
 dependencies {
     // okhttp for performing api calls
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation(libs.okhttp)
 
     // used for serialization/deserialization
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation(libs.kotlinx.serialization.json)
 
     // used for reading from/ writing to Word, Excel, PowerPoint files
-    implementation("org.apache.poi:poi-ooxml:5.2.5")
+    implementation(libs.apache.poi.ooxml)
 
     // used for working with coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.0")
+    implementation(libs.kotlinx.coroutines.core)
 
     // used for writing unit tests (this would normally go in the test module, by using testImplementation("package"))
-    implementation("junit:junit:4.13.2")
+    implementation(libs.junit)
 
     // used for logging in the console
-    implementation("org.slf4j:slf4j-api:2.0.7")
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation(libs.slf4j.api)
+    implementation(libs.logback.classic)
 }
